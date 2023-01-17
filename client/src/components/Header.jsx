fd112c5209d4d5a7b20b164b2cdba8f54be04b47
Rufat-J <rj.7@live.se>
2023-01-17 21:43:35 +0100

    close menu when clicking outside

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function () {
   const [openMenu, setOpenMenu] = useState(false);
   const toggleMenu = () => {
     setOpenMenu(!openMenu);
   };
+
+
+  //Close menu when click outside.
+  let menuRef = useRef();
+  useEffect(() => {
+    let handler = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setOpenMenu(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handler);
+
+    return () => {
+      document.removeEventListener('mousedown', handler)
+    }
+  });
+
+
   return (
     <>
       <header>
@@ -22,13 +41,15 @@ export default function () {
         </nav>
       </header>
       {openMenu ? (
-        <div className="dropdown-menu">
+        <div ref={menuRef} className="dropdown-menu">
           <a href="/">Home</a>
           <a href="#movies">Movies</a>
           <a href="#tickets">Tickets</a>
           <a href="calender">Calender</a>
         </div>
       ) : null}
+
+
     </>
   );
 }
