7628a646b19e5083e99b6312266bff8a0bcc6d7b
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-01-17 15:30:25 +0100

    added button

--- client/src/components/Mainpage.jsx
+++ client/src/components/Mainpage.jsx
@@ -8,8 +8,10 @@ export default function () {
         <div className="transparent-background">
           <h2>"WHATEVER YOU DO, <br></br> DON'T FALL ASLEEP."</h2>
         </div>
-        <button>LOGIN</button>
-        <button>SIGNUP</button>
+        <div className="center-btn">
+          <button className="btn">LOGIN</button>
+          <button className="btn">SIGNUP</button>
+        </div>
       </div>
     </section>
   );
