7ac167d0ff89fe0eb4aebbd2c27ac16c3c468431
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-01-17 15:08:45 +0100

    first commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,23 +3,25 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import './styles.css'
 import Mainpage from './components/Mainpage'
-import {createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from './components/Footer.jsx'
 import Calender from "./components/Calender.jsx"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Header />
-      <RouterProvider router={
-          createBrowserRouter(createRoutesFromElements(
-              <Route path={"/"}>
-                  <Route index element={<Mainpage/>}/>
-                  <Route path={"home"} element={<Mainpage/>}/>
-                  <Route path={"calender"} element={<Calender/>}/>
-              </Route>
-                  )
-                  )
-                  }/>
-<Footer />
+    <Header />
+    <RouterProvider router={
+      createBrowserRouter(createRoutesFromElements(
+        <Route path={"/"}>
+          <Route index element={<Mainpage />} />
+          <Route path={"home"} element={<Mainpage />} />
+          <Route path={"calender"} element={<Calender />} />
+        </Route>
+      )
+      )
+    } />
+    
+    <Footer />
   </React.StrictMode>,
 )
