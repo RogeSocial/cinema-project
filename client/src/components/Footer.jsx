ecb62627a8e9dd34000f49379e84ae00dba29896
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-01-17 14:58:33 +0100

    added styleing to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 
+
 export default function() {
     return <footer>
-        <p>Footer text and content here</p>
+        <div>
+            <h1 className={"codinema"}>Codinema</h1>
+            <p>I am a paragraph, Click here to add some extra text and stuff like that</p>
+        </div>
+        <div>
+            <h1>Contact</h1>
+            500 Terry francis street<br/>
+            San Fransisco CA 94158<br/>
+            info@mysite.se<br/>
+            tel: 123-456 789
+        </div>
+        <div>
+            <h1>Quick menu</h1>
+            <ul>
+                <li><a href={"/movies"}>Movies</a></li>
+                <li><a href={"/account"}>Account</a></li>
+                <li><a href={"/home"}>Home</a></li>
+            </ul>
+        </div>
+        <div>
+            <h1>Socials</h1>
+            <ul>
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>Twitter</li>
+                <li>YouTube</li>
+            </ul>
+        </div>
     </footer>
 
 }
