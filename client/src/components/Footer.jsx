e9efa40a897edf63c0fa7f82f2d68266dc8eff7f
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-01-17 16:25:08 +0100

    edited footer style

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,35 +2,40 @@ import React from "react";
 
 
 export default function() {
-    return <footer>
-        <div>
-            <h1 className={"codinema"}>Codinema</h1>
-            <p>I am a paragraph, Click here to add some extra text and stuff like that</p>
-        </div>
-        <div>
-            <h1>Contact</h1>
-            500 Terry francis street<br/>
-            San Fransisco CA 94158<br/>
-            info@mysite.se<br/>
-            tel: 123-456 789
-        </div>
-        <div>
-            <h1>Quick menu</h1>
-            <ul>
-                <li><a href={"/movies"}>Movies</a></li>
-                <li><a href={"/account"}>Account</a></li>
-                <li><a href={"/home"}>Home</a></li>
-            </ul>
-        </div>
-        <div>
-            <h1>Socials</h1>
-            <ul>
-                <li>Facebook</li>
-                <li>Instagram</li>
-                <li>Twitter</li>
-                <li>YouTube</li>
-            </ul>
-        </div>
-    </footer>
+    return <>
+    <div className={'line-container'}>
+        <hr/>
+    </div>
+        <footer>
+            <div>
+                <h1 className={"codinema"}>Codinema</h1>
+                <p>I am a paragraph, Click here to add some extra text and stuff like that. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div>
+                <h1>Contact</h1>
+                500 Terry francis street<br/>
+                San Fransisco CA 94158<br/>
+                info@mysite.se<br/>
+                tel: 123-456 789
+            </div>
+            <div>
+                <h1>Quick menu</h1>
+                <ul>
+                    <li><a href={"/movies"}>Movies</a></li>
+                    <li><a href={"/account"}>Account</a></li>
+                    <li><a href={"/home"}>Home</a></li>
+                </ul>
+            </div>
+            <div>
+                <h1>Socials</h1>
+                <ul>
+                    <li><a href={"http://www.facebook.com"} target={'blank'}>Facebook</a></li>
+                    <li><a href={"http://www.instagram.com"} target={'blank'}>Instagram</a></li>
+                    <li><a href={"http://www.twitter.com"} target={'blank'}>Twitter</a></li>
+                    <li><a href={"http://www.youtube.com"} target={'blank'}>Youtube</a></li>
+                </ul>
+            </div>
 
+    </footer>
+    </>
 }
