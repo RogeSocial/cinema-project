d09b5235fbbee9c3c7289c2a842ab74c9731e227
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-01-18 15:45:10 +0100

    First commit

--- client/src/components/Mainpage.jsx
+++ client/src/components/Mainpage.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import '../CSS/home.css'
 
-export default function() {
+export default function () {
   return (
-    <section>
-      <div className="background-image">
-        <div className="transparent-background">
-          <h2>"WHATEVER YOU DO, <br></br> DON'T FALL ASLEEP."</h2>
+    <div>
+      <section>
+        <div className="background-image">
+          <div className="transparent-background">
+            <h2>"WHATEVER YOU DO, <br></br> DON'T FALL ASLEEP."</h2>
+          </div>
+          <div className="center-btn">
+            <button className="btn">LOGIN</button>
+            <button className="btn">SIGNUP</button>
+          </div>
         </div>
-        <div className="center-btn">
-          <button className="btn">LOGIN</button>
-          <button className="btn">SIGNUP</button>
-        </div>
-      </div>
-    </section>
+      </section>
+      <section className="section-top-movies">
+        <h1 className="title">Our top movies</h1>
+      </section>
+    </div>
   );
 }
