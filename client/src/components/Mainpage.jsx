e46b2f9f05f77f7499b6e3439022ef6ff2fb75f4
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-01-19 10:56:26 +0100

    Added movies to top movies section

--- client/src/components/Mainpage.jsx
+++ client/src/components/Mainpage.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import '../CSS/home.css'
+import MovieList from "./MovieList";
+
 
 export default function () {
+  const [movies] = useState([
+    {
+      id: 1,
+      title: "IT",
+      image:
+        "https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/09/06194231/it-capitulo-dos-1.jpg"
+    },
+    {
+      id: 2,
+      title: "IT",
+      image:
+        "https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/09/06194231/it-capitulo-dos-1.jpg"
+    },
+    {
+      id: 3,
+      title: "IT",
+      image:
+        "https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/09/06194231/it-capitulo-dos-1.jpg"
+    },
+  ]);
   return (
     <div>
       <section>
@@ -17,6 +39,12 @@ export default function () {
       </section>
       <section className="section-top-movies">
         <h1 className="title">Our top movies</h1>
+        <div className="top-movies">
+          <MovieList movies={movies} />
+        </div>
+        <div className="center-btn">
+        <button className="more-movies">MORE MOVIES</button>
+        </div>
       </section>
     </div>
   );
