e84bd0e58173371b360ae7652eb0cc469ca7a5e7
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-01-20 14:47:25 +0100

    ticket form edited css and deleted form items

--- client/src/components/TicketForm.jsx
+++ client/src/components/TicketForm.jsx
@@ -4,48 +4,42 @@ import ticketImage from '../images/ticket.jpg';
 
 export default function TicketForm(props) {
 
-
-
     console.log(`The props are currently ${props}`);
 
     return (
         <div className={"ticket-form-page"}>
             <div className={"ticket-form-box"}>
+                <div className={"form-row"}>
                 <div className={"movie-details"}>
                     <h1>Name of the movie (props.movieName) {props.moviename}</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, magna vel dictum porta,
                         risus massa commodo dui, id vestibulum risus ipsum eu risus.</p>
-
-
                 </div>
-                <div className={"form-row"}>
-                    <form>
+                <form className={"ticket-form"}>
+                    <h3>Purchase tickets</h3>
 
-                        <ul>
-                            <li>The chosen date {props.date}</li>
-                            <li>The chosen time {props.time}</li>
-                            <li>Movie theatre adress</li>
-                        </ul>
+                    <ul className='ticket-list'>
+                        <li>The chosen date {props.date}</li>
+                        <li>The chosen time {props.time}</li>
+                        <li>Movie theatre adress</li>
+                        <li>Ticket price</li>
+                    </ul>
 
-                        <label htmlFor={"phone"}>Phone:</label>
-                        <input type={"tel"} id={"phone"} className={"form-item"}/>
+                    <h3>Your contact information</h3>
+                    <ul className='ticket-list'>
+                        <li>Phone: user account number</li>
+                        <li>E-mail: user account e-mail</li>
+                    </ul>
 
-                        <label htmlFor={"email"}>E-mail:</label>
-                        <input type={"email"} id={"email"} className={"form-item"}/>
+                    <div className='ticket-picker-in-form'>
+                        Insert ticket-picker here
+                    </div>
 
-                        <label htmlFor={"number"} className={"ticket-number"}>Number of tickets:</label>
-                        <input type={"number"} className={"form-item"} min={"1"} max={"10"}/>
-
-                        <button>Choose seats</button>
-
-                        <button type={"submit"} className={"ticket-submit"}>Purchase tickets</button>
-                    </form>
-                    <div className={'image-box'}><img src={ticketImage} className={"ticket-image"} /></div>
-                </div>
+                    <button type={"submit"} className={"ticket-submit btn"}>Purchase tickets</button>
+                </form>
             </div>
-
         </div>
+    </div>
 
-    );
-
+);
 }
