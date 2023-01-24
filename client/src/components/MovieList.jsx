a9189fb70692351c9ee81b76eda47b93d20350fd
Johan <johan.moller040@gmail.com>
2023-01-24 17:40:25 +0100

    fixed issue with keyprops error

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -4,8 +4,8 @@ export default function ({ movies }) {
   return (
     <>
       {movies.map((movie) => (
-        <Link to={`${movie.id}/${movie.param}`}>
-          <div className="movie-card" key={movie.id}>
+        <Link to={`${movie.id}/${movie.param}`} key={movie.id}>
+          <div className="movie-card">
             <h3> {movie.title} </h3>
             <img src={movie.image} alt="a picture of a movie" />
             <h4>{showsMovieLength(movie)}</h4>
