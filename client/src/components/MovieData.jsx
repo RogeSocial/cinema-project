20526ecbc0239ff1607f1a0a2725e73049790687
Rufat-J <rj.7@live.se>
2023-01-24 14:26:03 +0100

    movie ticket-form background images and trailers

--- client/src/components/MovieData.jsx
+++ client/src/components/MovieData.jsx
@@ -5,8 +5,8 @@ export let movieArray = [
     trailer: 'cCfO1aB8CIE',
     text: "A group of young adults visit a boarded up campsite named Crystal Lake where they soon encounter the mysterious Jason Voorhees and his deadly intentions.",
     image:
-      "https://wallpapercave.com/wp/wp3940179.jpg",
-    date:[1,2,3,4,5,6,7,8,9,10],
+      "https://images5.alphacoders.com/442/442591.jpg",
+    date:[11,2,3,4,5,6,7,8,9,10],
     param: "Friday-the-13th",
     length: 120,
     background: "https://wallpapercave.com/wp/wp3940179.jpg"
@@ -15,9 +15,9 @@ export let movieArray = [
     id: 2,
     title: "Hellraiser",
     trailer: 'oUlgwJNdu2I',
-    text: "Hellraiser is a 1987 British supernatural horror film[6] written and directed by Clive Barker, and produced by Christopher Figg, based on Barker's 1986 novella The Hellbound Heart.",
+    text: "A take on Clive Barker's 1987 horror classic where a young woman struggling with addiction comes into possession of an ancient puzzle box, unaware that its purpose is to summon the Cenobites.",
     image:
-      "https://img.hulu.com/user/v3/artwork/eb138082-5c40-4a8f-aa30-3fc66ef615fd?base_image_bucket_name=image_manager&base_image=bb8c33aa-6af6-4303-8cb7-2aa598039a75&size=1600x900&format=jpeg",
+      "https://decider.com/wp-content/uploads/2022/10/HELLRAISER-HULU-REVIEW.jpg?quality=75&strip=all",
     date:[11,12,13,14,15,16,17,18,19,20],
     length: 122,
     param: "hellraiser",
@@ -27,8 +27,8 @@ export let movieArray = [
     id: 3,
     title: "It",
     trailer: 'xKJmEC5ieOk',
-    text: "Jacob's Ladder is a 1990 American psychological horror film[4] directed by Adrian Lyne, produced by Alan Marshall and written by Bruce Joel Rubin.",
-    image: "https://wallpaperaccess.com/full/37961.jpg",
+    text: "In the summer of 1989, a group of bullied kids band together to destroy a shape-shifting monster, which disguises itself as a clown and preys on the children of Derry, their small Maine town.",
+    image: "https://c4.wallpaperflare.com/wallpaper/149/736/981/it-clown-bill-skarsgard-horror-wallpaper-preview.jpg",
     date:[21,22,23,24,25,26,27,28,29,30],
     length: 124,
     param: "jacobs-ladder",
@@ -36,61 +36,72 @@ export let movieArray = [
   },
   {
     id: 4,
-    title: "The Shining",
+    title: "Paranormal Activity",
+    trailer: "zR2cc1BwdmI",
     text: "The Shining is a 1980 psychological horror film produced and directed by Stanley Kubrick and co-written with novelist Diane Johnson.",
     image:
-      "https://i.guim.co.uk/img/media/68a5a828e8ebf46df1b62fcb3f09ab78dcfbc33f/12_0_3440_2064/master/3440.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=bb24447c29573d2dcb7942acae72c56a",
+      "https://images.tbs.com/tbs/$dyna_params/https%3A%2F%2Fi.cdn.tbs.com%2Fassets%2Fimages%2F2019%2F10%2FParanormal-Activity-1920x1080.jpg",
     date:[31,2,14,21,28],
     length: 126,
     param: "the-shining",
-    background: "https://i.guim.co.uk/img/media/68a5a828e8ebf46df1b62fcb3f09ab78dcfbc33f/12_0_3440_2064/master/3440.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=bb24447c29573d2dcb7942acae72c56a"
+    background: "https://images.tbs.com/tbs/$dyna_params/https%3A%2F%2Fi.cdn.tbs.com%2Fassets%2Fimages%2F2019%2F10%2FParanormal-Activity-1920x1080.jpg"
   },
   {
     id: 5,
-    title: "The Mist",
-    text: "The Mist (also known as Stephen King's The Mist) is a 2007 American science-fiction horror film based on the 1980 novella The Mist by Stephen King. The film was written and directed by Frank Darabont.",
+    title: "The Nun",
+    trailer: 'G7kORFktHnM',
+    text: "A take on Clive Barker's 1987 horror classic where a young woman struggling with addiction comes into possession of an ancient puzzle box, unaware that its purpose is to summon the Cenobites.",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUMOJI5WGX4QxVYalQkhZCqv4RfNPPclbPsQ&usqp=CAU",
+      "https://w0.peakpx.com/wallpaper/307/417/HD-wallpaper-the-nun-movie-the-nun-movies-2018-movies.jpg",
     date:[1,8,15,22,29],
     length: 128,
-    param: "the-mist"
+    param: "the-mist",
+    background: "https://images2.alphacoders.com/936/936582.jpg"
   },
   {
     id: 6,
-    title: "Land of the Dead",
+    title: "The Grudge",
+    trailer: 'O2NKzO-fxwQ',
     text: "Land of the Dead (also known as George A. Romero's Land of the Dead) is a 2005 post-apocalyptic horror film written and directed by George A. Romero; the fourth of Romero's six Living Dead movies, it is preceded by Night of the Living Dead, Dawn of the Dead and Day of the Dead, and succeeded by Diary of the Dead and Survival of the Dead.",
     image:
-      "https://thumbs.dreamstime.com/b/scary-tree-shadow-red-background-halloween-dark-creepy-forest-59589126.jpg",
+      "https://c4.wallpaperflare.com/wallpaper/502/400/889/dark-demon-film-grudge-wallpaper-preview.jpg",
     date:[3,10,17,24,31],
   length: 130,
-    param: "land-of-the-dead"
+    param: "land-of-the-dead",
+    background: 'https://r4.wallpaperflare.com/wallpaper/445/200/897/bunshinsaba-dark-evil-film-wallpaper-31b293f6a52789800ff3681dfeae5849.jpg'
   },
   {
     id: 7,
-    title: "The Ring",
+    title: "Rings",
+    trailer: 'uukQ_6szDm8',
     text: "The Ring is a 2002 American supernatural horror film directed by Gore Verbinski from a screenplay by Ehren Kruger, starring Naomi Watts, Martin Henderson, David Dorfman, Brian Cox, and Daveigh Chase.",
     image: 'https://wallpapercave.com/wp/wp4357758.jpg' ,
     date:[4,11,18,25,1],
     length: 132,
-    param: "the-ring"
+    param: "the-ring",
+    background: 'https://images2.alphacoders.com/824/824908.jpg'
   },
   {
     id: 8,
     title: "Scream",
+    trailer: 'beToTslH17s',
     text: "Scream is a 1996 American slasher film[5] directed by Wes Craven and written by Kevin Williamson. The film stars Neve Campbell, David Arquette, Courteney Cox, Matthew Lillard, Rose McGowan, Skeet Ulrich, Jamie Kennedy, and Drew Barrymore.",
     image:
       "https://4kwallpapers.com/images/wallpapers/scream-2022-movies-horror-movies-thriller-black-background-1284x2778-6756.jpg",
     date:[5,12,19,26,2],
     length: 134,
-    param: "scream"
+    param: "scream",
+    background: 'https://images7.alphacoders.com/122/1226941.jpg'
   },
   {
     id: 9,
     title: "Ouija: Origin of Evil",
+    trailer: 'MJIcZGEjjwo',
     text: "Ouija: Origin of Evil is a 2016 American supernatural horror film directed and edited by Mike Flanagan and written by Flanagan and Jeff Howard. The film is a prequel to the 2014 film Ouija and stars Elizabeth Reaser, Annalise Basso, and Henry Thomas.",
-    image: "https://images3.alphacoders.com/808/thumb-1920-808740.jpg",
+    image: "https://www.hdwallpapers.in/thumbs/2016/ouija_origin_of_evil_4k-t2.jpg",
     date:[6,13,20,27,3],
     length: 136,
-    param: "ouija-origin-of-evil"
+    param: "ouija-origin-of-evil",
+    background: 'https://cdn-cc-image-api-p001.cc.vubiquity.com/sf-api/image?i=https://vucscdnprodne.azureedge.net/images/vmest_prod/1/furniture/VX-5841161_1920x1080.PNG?q=80'
   },
 ];
