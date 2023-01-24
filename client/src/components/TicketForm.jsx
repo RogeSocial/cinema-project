c4dd5b1db5e2c9b1588642600e3327913f116e87
Rufat-J <rj.7@live.se>
2023-01-24 16:20:00 +0100

    ticket-form page

--- client/src/components/TicketForm.jsx
+++ client/src/components/TicketForm.jsx
@@ -43,30 +43,20 @@ export default function TicketForm() {
                     <p className='movieInfo'>Stars: {movie.stars}</p>
                     <p className="movieInfo">Length: {movie.length}m</p>
                     <p className="movieInfo">Release date: {movie.release}</p>
-                </div>
-
-                <form className={"ticket-form"}>
+                    <form className={"ticket-form"}>
                     <h3>Purchase tickets</h3>
 
                     <ul className='ticket-list'>
                         <li>Available dates:  </li>
                         { dateSelector()}
                         <li>The chosen time </li>
-                        <li>Ticket price</li>
                     </ul>
-
-                    <h3>Your contact information</h3>
-                    <ul className='ticket-list'>
-                        <li>Phone: user account number</li>
-                        <li>E-mail: user account e-mail</li>
-                    </ul>
-
-                    <div className='ticket-picker-in-form'>
-                        Insert ticket-picker here
-                    </div>
                     
                     <button  type={"submit"} className={"ticket-submit btn "}>Chose seats</button>
                 </form>
+                </div>
+
+                
 
         </div>
     </div>
