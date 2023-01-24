0585e62506c1acda2d824f6fb3bd6a7dd3f49102
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2023-01-24 23:47:18 +0100

    seat with section

--- client/src/components/Reserve.jsx
+++ client/src/components/Reserve.jsx
@@ -3,7 +3,6 @@ import '../CSS/reserve.css'
 
 function Seats() {
     const [seats, setSeats] = useState([]);
-    const [totalCost, setTotalCost] = useState();
     const [message, setMessage] = useState('Select your seat');
 
     // Initialize the seats state variable
@@ -15,7 +14,12 @@ function Seats() {
                 if (i * 8 + j + 1 <= 13) {
                     occupied = true;
                 }
-                newSeats.push({ id: i * 8 + j + 1, seat: j + 1, row: i + 1, occupied: occupied, selected: false, available: true });
+                let plusSeat = false;
+                if(i === 3){
+                    plusSeat = true;
+                    console.log("plusSeat");
+                }
+                newSeats.push({ id: i * 8 + j + 1, seat: j + 1, row: i + 1, occupied: occupied, selected: false, available: true, plusSeat: plusSeat});
             }
         }
         setSeats(newSeats);
@@ -25,20 +29,29 @@ function Seats() {
         let element = event.target;
         let updatedSeats = [...seats];
         let selectedSeat = updatedSeats.find(seat => seat.id === parseInt(element.id));
+        let normalPrice = 0;
+        let plusPrice = 0;
+        if(selectedSeat.plusSeat){
+            plusPrice = 12;
+            console.log("plus");
+        }
+        else{
+            normalPrice = 10;
+            console.log("normal");
+        }
         if(!selectedSeat.occupied){
             selectedSeat.selected = !selectedSeat.selected;
             setSeats(updatedSeats);
-            let selectedSeats = updatedSeats.filter(seat => seat.selected === true);
+
+            let selectedSeats = updatedSeats.filter(seat => seat.selected === true && seat.plusSeat === false);
             let selectedSeatsCount = selectedSeats.length;
+
+            let selectedSeatsPlus = updatedSeats.filter(seat => seat.selected === true && seat.plusSeat === true);
+            let selectedSeatsPlusCount = selectedSeatsPlus.length;
+
             // Update the message displayed in the "message" div
-            
-            setMessage(`You have selected ${selectedSeatsCount} seats -  ${selectedSeatsCount * 10}$`);
-            // Update the total cost
-            if (selectedSeat.selected) {
-                setTotalCost(totalCost + 10);
-            } else {
-                setTotalCost(totalCost - 10);
-            }
+            console.log("normal: " + selectedSeatsCount + " plus: "+ selectedSeatsPlusCount);
+            setMessage(`You have selected ${selectedSeatsCount + selectedSeatsPlusCount} seats ${(selectedSeatsCount * 10) + (selectedSeatsPlusCount * 12)}$`);
         }
     }
 
@@ -48,7 +61,7 @@ function Seats() {
             <div className="seatsWrap">
 
                 {seats.map((seat) => (
-                    <div key={seat.id} className={`seat ${seat.occupied ? 'occupied' : seat.selected ? 'selected' : 'available'}`} id={seat.id} onClick={handleClick}></div>
+                    <div key={seat.id} className={`seat ${seat.occupied ? 'occupied' : seat.selected ? 'selected' : seat.plusSeat ? 'plusSeat' : 'available'}`} id={seat.id} onClick={handleClick}></div>
                 ))}
             </div>
           
