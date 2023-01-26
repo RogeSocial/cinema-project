7aef65369638cecc468dc8e0f278af00370f4c45
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-01-26 20:37:07 +0100

    made the two quote-arrays into one array of quote-objects

--- client/src/components/Quotes.jsx
+++ client/src/components/Quotes.jsx
@@ -1,65 +1,25 @@
 import '../styles/home.css';
 import '../styles/quotes.css';
-import React, { useState, useEffect } from 'react';
-
-//The elements of both arrays must be kept in the right order
-// because the index of each horrorQuote corresponds to the index of each quoteData
-const horrorQuote = [
-    "\"Here's Johnny!\"",
-    "\"I see dead people\"",
-    "\"They're here!",
-    "\"Do you like scary movies?\"",
-    "\"We all go a little mad sometimes\"",
-    "\"It's alive! It's alive!\"",
-    "\"They're not gonna catch themselves\"",
-    "\"I'm your boogie man\"",
-    "\"Whatever you do, don’t fall asleep\"",
-    "\"I want to play a game\"",
-    "\"The power of Christ compels you!\"",
-    "\"Tell her to stay away from the light\"",
-    "\"Be afraid. Be very afraid\"",
-    "\"I'm having an old friend for dinner\""
-];
-
-const quoteData = [
-    "– The Shining, 1980",
-    "– The Sixth Sense, 1999",
-    "– Poltergeist, 1982",
-    "– Scream, 1996",
-    "– Psycho, 1960",
-    "– Frankenstein, 1931",
-    "– Evil Dead II, 1987",
-    "– A Nightmare on Elm Street, 1984",
-    "– A Nightmare on Elm Street, 1984",
-    "– Saw, 2004",
-    "– The Exorcist, 1973",
-    "– Poltergeist, 1982",
-    "– The Fly, 1986",
-    "– The Silence of the Lambs, 1991"
-];
+import React, {useEffect, useState} from 'react';
+import {quoteArray} from "./QuoteData.jsx";
 
 export default function () {
-
-    const [index, setIndex] = useState(Math.floor(Math.random() * horrorQuote.length));
+    const [index, setIndex] = useState(Math.floor(Math.random() * quoteArray.length));
     const animationRef = React.createRef();
 
     useEffect(() => {
-        if (animationRef.current) {
-            animationRef.current.addEventListener("animationiteration", handleIteration);
-        }
+        animationRef.current.addEventListener("animationiteration", handleIteration);
         return () => {
-            if (animationRef.current) {
-                animationRef.current.removeEventListener("animationiteration", handleIteration);
-            }
+            animationRef.current.removeEventListener("animationiteration", handleIteration);
         }
     }, [animationRef]);
 
     const handleIteration = (event) => {
-        setIndex(Math.floor(Math.random() * horrorQuote.length));
+        setIndex(Math.floor(Math.random() * quoteArray.length));
     }
 
-    let movieQuote = horrorQuote[index];
-    let byline = quoteData[index];
+    let movieQuote = quoteArray[index].quote;
+    let byline = quoteArray[index].quoteSource;
 
     return (
         <div className={"quotebox"}>
