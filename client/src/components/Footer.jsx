236d7b32752c213c455e290969fe07efa6d55e43
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-01-26 17:20:32 +0100

    removed classname and cleand css-file

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function () {
     return <footer>
         <hr id={"footer-line"} />
         <div className={"footer-content"}>
-            <h2 className={"codinema"}>Codinema</h2>
+            <h2>Codinema</h2>
             <p>Codinema is the ultimate cinema for horror movie fans. Our state-of-the-art theater boasts
                 high-definition projectors, surround sound, and comfortable seating, immersing you in a truly terrifying
                 experience.</p>
