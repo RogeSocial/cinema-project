a4beb8188238ab9390b52707cd83e0fbcf46e31c
SimonLeBuono123 <simon.villoresi@gmail.com>
2023-01-27 10:55:54 +0100

    Added new route type "error element" to main.jsx. Also changed name of pagenotfound
    new name of pageNotFound is errorBoundary

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import Mainpage from './pages/Mainpage.jsx'
 import {
 
     Route,
-     RouterProvider,
+    RouterProvider,
     createBrowserRouter,
     createRoutesFromElements
 } from "react-router-dom";
@@ -17,32 +17,36 @@ import Reservations from './pages/Reservations'
 
 import TicketForm from "./pages/TicketForm.jsx";
 import Reserve from "./pages/Reserve.jsx";
-import PageNotFound from "./pages/pageNotFound.jsx";
+import ErrorBoundary from "./pages/errorBoundary.jsx";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-       <RouterProvider router={
-           createBrowserRouter(
-               createRoutesFromElements(
-                       <Route path="/" element={<App/>}>
-                       <Route index element={<Mainpage/>}/>
-                       <Route path="/:id/:param" element={<TicketForm/>}/>
-                       <Route path="/tickets/:id/:param" element={<TicketForm/>}/>
-                       <Route path="movies" element={<Movies/>}/>
-                       <Route path="movies/:id/:param" element={<TicketForm/>}/>
-                       <Route path="tickets" element={<Tickets/>}/>
-                       <Route path="reservations" element={<Reservations/>}/>
-                       <Route path="*" element={<PageNotFound/>}/>
-                       <Route path="reserve" element={<Reserve/>}/>
-                       <Route path="ticket-form" element={<TicketForm/>}/>
-                       <Route path="user">
-                           <Route path="login"/>
-                           <Route path="register"/>
-                       </Route>
-                       </Route>
-               )
-           )
-       }/>
+        <RouterProvider router={
+            createBrowserRouter(
+                createRoutesFromElements(
+                    <Route
+                        path="/"
+                        element={<App/>}
+                        errorElement={<ErrorBoundary/>}>
+                        
+                        <Route index element={<Mainpage/>}/>
+                        <Route path="/:id/:param" element={<TicketForm/>}/>
+                        <Route path="/tickets/:id/:param" element={<TicketForm/>}/>
+                        <Route path="movies" element={<Movies/>}/>
+                        <Route path="movies/:id/:param" element={<TicketForm/>}/>
+                        <Route path="tickets" element={<Tickets/>}/>
+                        <Route path="reservations" element={<Reservations/>}/>
+                        <Route path="*" element={<ErrorBoundary/>}/>
+                        <Route path="reserve" element={<Reserve/>}/>
+                        <Route path="ticket-form" element={<TicketForm/>}/>
+                        <Route path="user">
+                            <Route path="login"/>
+                            <Route path="register"/>
+                        </Route>
+                    </Route>
+                )
+            )
+        }/>
     </React.StrictMode>
 );
---
+++ client/src/pages/errorBoundary.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../styles/pageNotFound.css'
+import {isRouteErrorResponse, useRouteError} from "react-router-dom";
+
+function ErrorBoundary() {
+    const error = useRouteError();
+    if(isRouteErrorResponse(error)){
+        if(error.status === 404){
+            return <div className={"error"}>
+                <h2>Error 404</h2>
+                <h3> This page doesn't exist!</h3>
+            </div>
+        }
+        if (error.status === 401) {
+            return <div className={"error"}>
+                <h2>Error 401</h2>
+                <h3>You aren't authorized to see this</h3></div>;
+        }
+
+        if (error.status === 503) {
+            return <div className={"error"}>
+                <h2>Error 503</h2>
+                <h3>Looks like our API is down</h3></div>;
+        }
+
+        if (error.status === 418) {
+            return <div className={"error"}>
+                <h2>Error 418</h2>
+                🫖</div>;
+        }
+    }
+    return <div className={"error"}>
+        <h3>Something went wrong</h3>
+    </div>
+}
+
+export default ErrorBoundary;
