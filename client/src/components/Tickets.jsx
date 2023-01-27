48ab78977224b2b0b1bfd78582bf06a727de4b2a
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2023-01-27 13:24:51 +0100

    cleaned up tickets, broke out components

--- client/src/components/Tickets.jsx
+++ client/src/components/Tickets.jsx
@@ -2,24 +2,25 @@ import "../styles/tickets.css"
 import "../styles/movies.css"
 import "../styles/home.css"
 
-import { useEffect, useRef, useState } from "react";
-import { movieArray } from "./MovieData.jsx";
+import {useEffect, useRef, useState} from "react";
+import {movieArray} from "./MovieData.jsx";
 import MovieList from "./MovieList.jsx";
 
-let dates = [];
 const daysForward = 21;
 let dateString = null;
-let dateInNumbers = [{ date: null, month: null, weekDay: null }];
-
+let dateInNumbers = [{date: null, month: null, weekDay: null}];
 let daysInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 export default function () {
-    { dates = [] }
     const [open, setOpen] = useState(false);
-    const [movies] = useState(movieArray);
 
     let calenderRef = useRef();
 
+    {
+        calculateCurrentDate();
+    }
+
+    //this part will handle clicking outside the dropdown menu, so it closes
     useEffect(() => {
         let handler = (e) => {
             if (!calenderRef.current.contains(e.target)) {
@@ -32,61 +33,98 @@ export default function () {
         }
     });
 
+    //shows the current selected date
+    const SelectedDateComp = () => (
+        <div id="selectedDate">
+            {dateString}
+        </div>
+    );
 
-    //compare the selected date and matches with the "database"(movieData)" and returns the one who are matched
-    function moviesOnDate() {
-        let tmpArray = [];
-        for (let i = 0; i < movieArray.length; i++) {
-            for (let j = 0; j < movieArray[i].date.length; j++) {
-                if (movieArray[i].date[j] === dateInNumbers.date) {
-                    tmpArray.push(movieArray[i]);
-                }
-            }
-        }
-        return tmpArray;
-    }
+    const SetDateComp = ({item}) => (
+        <div onClick={function (e) {
+            setDateString(item.day, item.month, returnDayName(item.dayName))
+        }}>
+            {item.day} / {item.month} - {returnDayName(item.dayName)}
+        </div>
+    );
+
+    const UlListComp = () => (
+        <ul onClick={() => {
+            setOpen(!open)
+        }}>
+            {calculateCurrentDate().map((item) =>
+                <div className="calender-list" key={item.id}>
+                    {/*{setDateComp(item)}*/}
+                    {/*<SetDateComp item = {item} />*/}
+                    <SetDateComp item = {item} />
+                </div>
+            )}
+        </ul>
+    );
+
+    const CalenderDropDownComp = () => (
+        <div className={`calender-dropdown-trigger ${open ? 'active' : 'inactive'}`}>
+            <UlListComp/>
+        </div>
+    );
+
+    const ButtonComp = () => (
+        <button className="button" onClick={() => {
+            setOpen(!open)
+        }}>Pick a Date
+        </button>
+    );
+
+    const TitleComp = () => (
+        <h2 id="buyTicketsTitle">Buy Tickets</h2>
+    );
+
+    //for the button and the dropdown menu to choose date
+    const CalenderBoxComp = () => (
+        <div id="calender-box" ref={calenderRef}>
+            <ButtonComp/>
+            <CalenderDropDownComp/>
+        </div>
+    );
 
     return <section id="calenderSection" className="calender">
         <div className="wrap">
-
-            <div id="selectedDate">{dateString}</div>
-            <hr />
-            <h2 id="buyTicketsTitle">Buy Tickets</h2>
-            <hr />
-            <div id="calender-box" ref={calenderRef}>
-
-                <div id="btn" onClick={() => { setOpen(!open) }}>Pick a Date</div>
-
-                <div className={`calender-dropdown-trigger ${open ? 'active' : 'inactive'}`}>
-                    {calculateCurrentDate()}
-                    <ul onClick={() => { setOpen(!open) }}>
-                        {dates.map((item) =>
-
-                            <div className="calender-list" key={item.id}>
-                                <div onClick={function (e) { setDate(item.day, item.month, returnDayName(item.dayName)) }}>
-                                    {item.day} / {item.month} - {returnDayName(item.dayName)}
-                                </div>
-
-                            </div>
-                        )}
-                    </ul>
-                </div>
-            </div>
+            <SelectedDateComp/>
+            <hr/>
+            <TitleComp/>
+            <hr/>
+            <CalenderBoxComp/>
         </div>
         <div id="top-movies">
-            <MovieList movies={moviesOnDate()} />
+            <MovieList movies={moviesOnDate()}/>
         </div>
     </section>
 }
 
-function setDate(inDate, inMonth, inWeekDay) {
+//compare the selected date and matches with the "database"(movieData)" and returns the one who are matched
+function moviesOnDate() {
+    let tmpArray = [];
+    for (let i = 0; i < movieArray.length; i++) {
+        for (let j = 0; j < movieArray[i].date.length; j++) {
+            if (movieArray[i].date[j] === dateInNumbers.date) {
+                tmpArray.push(movieArray[i]);
+            }
+        }
+    }
+    return tmpArray;
+}
+
+//will set the string "dateString", composed of the date like this: date / month - weekday
+function setDateString(inDate, inMonth, inWeekDay) {
     dateInNumbers.date = inDate;
     dateInNumbers.month = inMonth;
     dateInNumbers.weekDay = inWeekDay;
     dateString = dateInNumbers.date + " / " + dateInNumbers.month + "-" + dateInNumbers.weekDay;
 }
 
+//calculates the current date and returns it in an array
 function calculateCurrentDate() {
+    let tmpArray = [];
     for (let i = 0; i < daysForward; i++) {
         let today = new Date();
         let dateLimit = new Date(new Date().setDate(today.getDate() + i));
@@ -96,8 +134,9 @@ function calculateCurrentDate() {
         object.year = dateLimit.getFullYear();
         object.dayName = dateLimit.getDay();
         object.id = i;
-        dates.push(object);
+        tmpArray.push(object);
     }
+    return tmpArray;
 }
 
 function returnDayName(inIndex) {
