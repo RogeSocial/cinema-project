19c97e584b7140a65903c590175bc2615983c095
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-01-30 20:38:50 +0100

    clean imports and MovieDetails fetch event

--- client/src/components/ChooseShow.jsx
+++ client/src/components/ChooseShow.jsx
@@ -2,17 +2,20 @@ import {dateSelector, timeSelector} from "./Utilities.jsx";
 import {Link} from "react-router-dom";
 import React from "react";
 
-export default function({currentMovie}) {
+export default function ({currentMovie}) {
     const movie = currentMovie;
 
     return <form className={"date-form"}>
         <h3>Purchase tickets</h3>
         <ul>
-            <li>Available dates:  </li>
+            <li>Available dates:</li>
             {dateSelector(movie)}
-            <li>The chosen time </li>
+            <li>The chosen time</li>
             {timeSelector(movie)}
         </ul>
-        <Link to="/reserve"><button type={"submit"} className={"ticket-submit button"}>Purchase tickets</button></Link>
+
+        <Link to="/reserve">
+            <button type={"submit"} className={"ticket-submit button"}>Purchase tickets</button>
+        </Link>
     </form>
 }
--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,27 +1,13 @@
-import React from 'react';
 import { useParams } from 'react-router-dom';
-import '../styles/movie-details.css'
-import { movieArray } from '../components/movie-data.js';
-import '../styles/home.css'
 import MovieInformation from "../components/MovieInformation.jsx";
 import ChooseShow from "../components/ChooseShow.jsx";
+import { movieArray } from '../components/movie-data.js';
+import '../styles/movie-details.css'
+import '../styles/home.css'
 
 export default function MovieDetails() {
     const movieId = useParams().id
     const movie = movieArray.find(m => m.id == movieId)
-
-    self.addEventListener('fetch', event => {
-        event.respondWith(
-            (async function () {
-                const preloadResponse = await event.preloadResponse;
-                if (preloadResponse) {
-                    return preloadResponse;
-                }
-
-                return fetch(event.request);
-            })()
-        );
-    });
 
     return (
         <div style={{ background: `url(${movie.background})`, backgroundSize: 'cover' }} className="movie-details-page">
