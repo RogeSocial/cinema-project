e627672f66506c8f13d52a2124d43f4f40b1845b
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-01-30 09:29:53 +0100

    first commit

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import './login.css'
+
+export default function() {
+    return(
+        <div>
+            
+        </div>
+    )
+}
