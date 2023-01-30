6ae141f15b2f1d179dced4e8d89ecfbdda069e75
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-01-30 20:19:14 +0100

    name change

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import '../styles/ticket-form.css'
+import '../styles/movie-details.css'
 import { movieArray } from '../components/movie-data.js';
 import '../styles/home.css'
 import MovieInformation from "../components/MovieInformation.jsx";
 import ChooseShow from "../components/ChooseShow.jsx";
 
-export default function TicketForm() {
+export default function MovieDetails() {
     const movieId = useParams().id
     const movie = movieArray.find(m => m.id == movieId)
 
@@ -17,13 +17,14 @@ export default function TicketForm() {
                 if (preloadResponse) {
                     return preloadResponse;
                 }
+
                 return fetch(event.request);
             })()
         );
     });
 
     return (
-        <div style={{ background: `url(${movie.background})`, backgroundSize: 'cover', }} className="ticket-form-page">
+        <div style={{ background: `url(${movie.background})`, backgroundSize: 'cover' }} className="movie-details-page">
             <div className={"content-frame"}>
                 <MovieInformation currentMovie={movie} />
                 <ChooseShow currentMovie={movie} />
