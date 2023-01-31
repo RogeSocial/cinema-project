3810b824be9ed28f0d53e7329ade580f8ad59fdc
Rufat-J <rj.7@live.se>
2023-01-31 13:54:25 +0100

    changed name of showcase seats.

--- client/src/pages/ReservePage.jsx
+++ client/src/pages/ReservePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../styles/reservePage.css'
-import ShowCaseSeats from '../components/ShowCaseSeats';
+import SeatTypes from '../components/ShowCaseSeats';
 import TheaterSeats from '../components/TheaterSeats';
 
 export default function () {
     return (<section className='reserveSection'>
-        <ShowCaseSeats />
+        <SeatTypes />
         <div className="screen"></div>
         <TheaterSeats />
     </section>
