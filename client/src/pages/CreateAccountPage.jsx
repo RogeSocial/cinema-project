51f0eeac109e6b1ecf64092ac36c5586e19db93f
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-02-01 13:20:17 +0100

    soloved feedback

--- client/src/pages/CreateAccountPage.jsx
+++ client/src/pages/CreateAccountPage.jsx
@@ -4,22 +4,22 @@ import '../styles/account.css'
 
 export default function () {
     return (
-        <section>
-        <form className="cover">
-            <label htmlFor="fullname">Fullname:</label>
-            <input type="text" placeholder="full name"></input>
-            <label htmlFor="email">Email:</label>
-            <input type="email" placeholder="email"></input>
-            <label htmlFor="phone number">Phone number:</label>
-            <input onKeyPress={(e) => !/[0-9 + "+"]/.test(e.key) && e.preventDefault()} placeholder="phone number"></input>
-            <label htmlFor="password">Password:</label>
-            <input type="password" placeholder="password" minLength="8"></input>
-            <label htmlFor="confirm password">Confirm password: </label>
-            <input type="password" placeholder="confirm password" minLength="8"></input>
-            <br></br>
-            <button className="button">REGISTER</button>
-            <Link to="/login"><button id="button">Already have an account? Sign in.</button></Link>
-        </form>
-    </section>
+        <section className="account">
+                <form className="cover">
+                    <label htmlFor="fullname">Fullname:</label>
+                    <input type="text" placeholder="full name"></input>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" placeholder="email"></input>
+                    <label htmlFor="phone number">Phone number:</label>
+                    <input onKeyPress={(e) => !/[0-9 + "+"]/.test(e.key) && e.preventDefault()} placeholder="phone number"></input>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" placeholder="password" minLength="8"></input>
+                    <label htmlFor="confirm password">Confirm password: </label>
+                    <input type="password" placeholder="confirm password" minLength="8"></input>
+                    <br></br>
+                    <button className="button">REGISTER</button>
+                    <Link to="/login"><button className="button-text">Already have an account? Sign in.</button></Link>
+                </form>
+        </section>
     )
 }
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -4,23 +4,24 @@ import '../styles/account.css'
 
 export default function () {
     
-    const [popupStyle, showPopup] = useState("hide");
+    const [popupStyle, showPopup] = useState("hide-popup");
     
     const popup = () => {
         showPopup("login-popup")
-        setTimeout(() => showPopup("hide"), 30000)
+        setTimeout(() => showPopup("hide-popup"), 30000)
     }
     
     return (
-        <section>
+        <section className="account">
             <form className="cover">
                 <label htmlFor="email">Email:</label>
                 <input type="email" placeholder="email"></input>
                 <label htmlFor="password">Password:</label>
                 <input type="password" placeholder="password" minLength="8"></input>
-                <div className="button-container">
-                    <button className="button" onClick={popup}>LOGIN</button>
-                    <Link to ="/register"><button className="button">REGISTER</button></Link>
+                <br></br>
+                <div className="button-group">
+                <button className="button" onClick={popup}>LOGIN</button>
+                <Link to ="/register"><button className="button-text">Don't have an account? Register here.</button></Link>
                 </div>
 
                 <div className={popupStyle}>
