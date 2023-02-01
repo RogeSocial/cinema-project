c8464fc32c1bc141b42e889e05f0b7f1049b8591
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2023-02-01 08:39:46 +0100

    final

--- client/src/components/IsDropDownActive.jsx
+++ client/src/components/IsDropDownActive.jsx
@@ -1,7 +1,6 @@
 import {PickADateList} from "./PickADateList.jsx";
 
 export const IsDropDownActive = ({open, openDatePicker, calculateCurrentDate}) => (
-    console.log("value of open: " + open),
         <div onClick={openDatePicker} className={`calender-dropdown-trigger ${open ? 'active' : 'inactive'}`}>
             <PickADateList setOpen={openDatePicker} calculateCurrentDate={calculateCurrentDate}/>
         </div>
