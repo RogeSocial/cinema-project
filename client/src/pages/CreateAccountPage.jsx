902f3aa1553de93af73fcd9856382248f039c037
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-02-02 21:49:26 +0100

    email validator function added

--- client/src/pages/CreateAccountPage.jsx
+++ client/src/pages/CreateAccountPage.jsx
@@ -4,40 +4,53 @@ import '../styles/account.css'
 import icon from "../images/apple-touch-icon.png";
 
 export default function () {
-    
+
+
+    // const registerForm = document.getElementById("formData");
+    // registerForm.submit();
+
     function checkPassword() {
         const password = document.getElementById("password").value;
         const confirm = document.getElementById("confirmPassword").value;
 
-        if (password === confirm) {
-            const registerForm = document.getElementById("formData");
-            registerForm.submit(); 
-        } else if (password != confirm) {
+        if (password.match(confirm)) {
+            return true;
+        } else {
             return false;
         }
     }
 
+    function checkEmail() {
+        const email = document.getElementById("email").value;
+        const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if (email.match(regex)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
     return (
         <section className="account">
-                <form id="formData" action="#" className="cover createAccountCover">
-                    <img className="loginImg accountImg" src={icon} alt="" />
-                    <label htmlFor="fullname">Fullname:</label>
-                    <input type="text" id="name" name="name" placeholder="full name"></input>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="email"></input>
-                    <label htmlFor="phone number">Phone number:</label>
-                    <input onKeyPress={(e) => !/[0-9 + "+"]/.test(e.key) && e.preventDefault()} placeholder="phone number" id="tel" name="tel"></input>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" placeholder="password" name="password" minLength="8" id={"password"}></input>
-                    <label htmlFor="confirm password">Confirm password: </label>
-                    <input type="password" placeholder="confirm password" minLength="8" id={"confirmPassword"} name="confirmPassword"></input>
-                    <br/>
-                    <div className="button-group">
-                    <button type="button" className="button" onClick={checkPassword}>REGISTER</button>
+            <form id="formData" action="#" className="cover createAccountCover">
+                <img className="loginImg accountImg" src={icon} alt="" />
+                <label htmlFor="fullname">Fullname:</label>
+                <input type="text" id="name" name="name" placeholder="full name"></input>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="email"></input>
+                <label htmlFor="phone number">Phone number:</label>
+                <input onKeyPress={(e) => !/[0-9 + "+"]/.test(e.key) && e.preventDefault()} placeholder="phone number" id="tel" name="tel"></input>
+                <label htmlFor="password">Password:</label>
+                <input type="password" placeholder="password" name="password" minLength="8" id={"password"}></input>
+                <label htmlFor="confirm password">Confirm password: </label>
+                <input type="password" placeholder="confirm password" minLength="8" id={"confirmPassword"} name="confirmPassword"></input>
+                <br />
+                <div className="button-group">
+                    <button type="button" className="button" onClick={checkEmail}>REGISTER</button>
                     <Link to="/login"><button className="button-text">Already have an account? Sign in.</button></Link>
-                    </div>
-                </form>
-                
+                </div>
+            </form>
         </section>
     )
 }
