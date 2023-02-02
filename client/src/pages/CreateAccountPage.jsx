27d30c5c13b415dab00deb606a5ab3e2287004ce
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-02-02 16:31:17 +0100

    Password checker added

--- client/src/pages/CreateAccountPage.jsx
+++ client/src/pages/CreateAccountPage.jsx
@@ -4,26 +4,40 @@ import '../styles/account.css'
 import icon from "../images/apple-touch-icon.png";
 
 export default function () {
+    
+    function checkPassword() {
+        const password = document.getElementById("password").value;
+        const confirm = document.getElementById("confirmPassword").value;
+
+        if (password === confirm) {
+            const registerForm = document.getElementById("formData");
+            registerForm.submit(); 
+        } else if (password != confirm) {
+            return false;
+        }
+    }
+
     return (
         <section className="account">
-                <form className="cover createAccountCover">
+                <form id="formData" action="#" className="cover createAccountCover">
                     <img className="loginImg accountImg" src={icon} alt="" />
                     <label htmlFor="fullname">Fullname:</label>
-                    <input type="text" placeholder="full name"></input>
+                    <input type="text" id="name" name="name" placeholder="full name"></input>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" placeholder="email"></input>
+                    <input type="email" id="email" name="email" placeholder="email"></input>
                     <label htmlFor="phone number">Phone number:</label>
-                    <input onKeyPress={(e) => !/[0-9 + "+"]/.test(e.key) && e.preventDefault()} placeholder="phone number"></input>
+                    <input onKeyPress={(e) => !/[0-9 + "+"]/.test(e.key) && e.preventDefault()} placeholder="phone number" id="tel" name="tel"></input>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" placeholder="password" minLength="8"></input>
+                    <input type="password" placeholder="password" name="password" minLength="8" id={"password"}></input>
                     <label htmlFor="confirm password">Confirm password: </label>
-                    <input type="password" placeholder="confirm password" minLength="8"></input>
+                    <input type="password" placeholder="confirm password" minLength="8" id={"confirmPassword"} name="confirmPassword"></input>
                     <br/>
                     <div className="button-group">
-                    <button className="button">REGISTER</button>
+                    <button type="button" className="button" onClick={checkPassword}>REGISTER</button>
                     <Link to="/login"><button className="button-text">Already have an account? Sign in.</button></Link>
                     </div>
                 </form>
+                
         </section>
     )
 }
