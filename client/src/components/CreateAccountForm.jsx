55bada389e3cbd515acb0d1092d197f3aa6a77ff
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-02-07 13:18:00 +0100

    added required for all the input fields

--- client/src/components/CreateAccountForm.jsx
+++ client/src/components/CreateAccountForm.jsx
@@ -20,17 +20,17 @@ export default function () {
         <form onSubmit={submit} id="formData" action="/login" className="cover createAccountCover">
             <img className="loginImg accountImg" src={icon} alt="" />
             <label htmlFor="fullname">Fullname:</label>
-            <input type="text" id="name" name="name" placeholder="full name"></input>
+            <input type="text" id="name" name="name" placeholder="full name" required="required"></input>
             <label htmlFor="email">Email:</label>
             <input type="email" id="email" name="email" placeholder="email"
-             value={email} onChange={(event)=>setEmail(event.target.value)}></input>
+            required="required" value={email} onChange={(event)=>setEmail(event.target.value)}></input>
             <label htmlFor="phone number">Phone number:</label>
-            <input onKeyPress={(e) => !/[0-9 + "+"]/.test(e.key) && e.preventDefault()} placeholder="phone number" id="tel" name="tel"></input>
+            <input onKeyPress={(e) => !/[0-9 + "+"]/.test(e.key) && e.preventDefault()} required="required" placeholder="phone number" id="tel" name="tel"></input>
             <label htmlFor="password">Password:</label>
-            <input type="password" placeholder="password" name="password" minLength="8" id={"password"}
+            <input type="password" required="required" placeholder="password" name="password" minLength="8" id={"password"}
              value={password} onChange={(event)=>setPassword(event.target.value)}></input>
             <label htmlFor="confirm password">Confirm password: </label>
-            <input type="password" placeholder="confirm password" minLength="8" id={"confirmPassword"} name="confirmPassword"></input>
+            <input type="password" required="required" placeholder="confirm password" minLength="8" id={"confirmPassword"} name="confirmPassword"></input>
             <br />
             <div className="button-group">
                 <button type="submit"className="button" onClick={handleRegister}>REGISTER</button>
