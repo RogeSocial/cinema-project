cbc09286a4cd0b4566a902b9fe0201b1f1ad190a
SimonLeBuono123 <simon.villoresi@gmail.com>
2023-02-07 15:10:35 +0100

    Added new sort function that sorts by id which it originally does.
    Added this function to home link so everytime you go to home it will sort by id as normally

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,6 +1,9 @@
 import {Link} from "react-router-dom";
-import React from "react";
+import React, {useContext} from "react";
+import globalContext from "../GlobalContext.jsx";
+import {unSortMovies} from "./movieSort.js";
 
 export default function () {
-    return <Link to={'/'}><h1>Codinema</h1></Link>
+    const {movies} = useContext(globalContext)
+    return <Link to={'/'} onClick={() => unSortMovies(movies)}><h1>Codinema</h1></Link>
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import {Link} from "react-router-dom";
-import React from "react";
+import React, {useContext} from "react";
+import globalContext from "../GlobalContext.jsx";
+import {unSortMovies} from "./movieSort.js";
 
 export default function () {
+
     return <nav>
         <Link to="movies" id='navbar'>Movies</Link>
         <Link to="tickets" id='navbar'>Tickets</Link>
