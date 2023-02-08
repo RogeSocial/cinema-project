00ac0690319c5c2f9279a64e9a12aba82a3b911d
Rufat-J <rj.7@live.se>
2023-02-09 00:31:18 +0100

    mirror update

--- client/src/components/FilterButton.jsx
+++ client/src/components/FilterButton.jsx
@@ -1,6 +1,6 @@
 export default function ({ handleClick, filterName }) {
   return (
-    <button className="fa-solid fa-filter button"  onClick={handleClick}>
+    <button className="button filterButton"  onClick={handleClick}>
       {filterName}
     </button>
   );
