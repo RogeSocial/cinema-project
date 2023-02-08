90844a9dbcbc3dfb8165d3a75083d5676501fdbb
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-02-08 15:30:37 +0100

    Added so everything updates at the same time

--- client/src/components/AccountComponents.jsx
+++ client/src/components/AccountComponents.jsx
@@ -1,45 +1,65 @@
-function checkPassword() {
+function validateInputs() {
     const password = document.getElementById("password").value;
     const confirm = document.getElementById("confirmPassword").value;
+    const email = document.getElementById("email").value;
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const fullname = document.getElementById("fullname").value;
+    let valid = true;
 
     if (confirm === '') {
         document.getElementById("pwrdc").innerHTML =
-        "Please confirm your password"
+            "Please confirm your password"
+        valid = false;
     } else if (confirm !== password) {
         document.getElementById("pwrdc").innerHTML =
-        "Password is not matching"
+            "Password is not matching"
+        valid = false;
     } else {
         document.getElementById("pwrdc").innerHTML =
-        ''
-        return true;
+            ''
     }
 
     if (password === '') {
-        document.getElementById("pwrd").innerHTML = 
-        'Please enter a password' 
+        document.getElementById("pwrd").innerHTML =
+            'Please enter a password'
+        valid = false;
     } else if (password.length < 8) {
         document.getElementById("pwrd").innerHTML =
-        'Please enter 8 characters'
+            'Please enter 8 characters'
+        valid = false;
     } else {
         document.getElementById("pwrd").innerHTML =
-        ''
-        return true;
+            ''
     }
-}
 
-function checkEmail() {
-    const email = document.getElementById("email").value;
-    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-    if (email.match(regex)) {
-        return true
+    if (email === '') {
+        document.getElementById("mail").innerHTML =
+            'Email is required'
+        valid = false;
+    } else if (!email.match(regex)) {
+        document.getElementById("mail").innerHTML =
+            'Provide a valid email address'
+        valid = false;
     } else {
-        return false;
+        document.getElementById("mail").innerHTML =
+            ''
     }
+
+
+    if (fullname === '') {
+        document.getElementById("name").innerHTML =
+            'Name is required'
+        valid = false;
+    } else {
+        document.getElementById("name").innerHTML =
+            ''
+    }
+
+    return valid;
 }
 
 export function handleRegister() {
-    if (checkPassword() === true && checkEmail() === true) {
+    if (validateInputs() === true) {
         const registerForm = document.getElementById("formData");
         registerForm.submit();
         return true;
--- client/src/components/CreateAccountForm.jsx
+++ client/src/components/CreateAccountForm.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import '../styles/account.css'
 import icon from "../images/apple-touch-icon.png";
@@ -21,23 +21,23 @@ export default function () {
     return <section className="account">
         <form onSubmit={submit} id="formData" action="/login" className="cover createAccountCover">
             <img className="loginImg accountImg" src={icon} alt="" />
-            <label htmlFor="fullname">Fullname:</label>
-            <input  type="text" id="name" name="name" placeholder="full name"
-                    value={fullName} onChange={event => setFullName(event.target.value)}></input>
-            <label htmlFor="email">Email:</label>
+            <label htmlFor="fullname" id="name">Fullname:</label>
+            <input type="text" id="fullname" name="name" placeholder="full name"
+                value={fullName} onChange={event => setFullName(event.target.value)}></input>
+            <label htmlFor="email" id="mail">Email:</label>
             <input type="email" id="email" name="email" placeholder="email"
-            required="required" value={email} onChange={(event)=>setEmail(event.target.value)}></input>
+                required="required" value={email} onChange={(event) => setEmail(event.target.value)}></input>
             <label htmlFor="phone number">Phone number:</label>
             <input onKeyDown={(e) => !/[0-9 + "+"]/.test(e.key) && e.preventDefault()} placeholder="phone number" id="tel" name="tel"
-            value={phoneNumber} onChange={event => setPhoneNumber(event.target.value)}></input>
+                value={phoneNumber} onChange={event => setPhoneNumber(event.target.value)}></input>
             <label htmlFor="password" id="pwrd">Password:</label>
             <input type="password" placeholder="password" name="password" minLength="8" id={"password"}
-             value={password} onChange={(event)=>setPassword(event.target.value)}></input>
+                value={password} onChange={(event) => setPassword(event.target.value)}></input>
             <label htmlFor="confirm password" id="pwrdc">Confirm password: </label>
             <input type="password" required="required" placeholder="confirm password" minLength="8" id={"confirmPassword"} name="confirmPassword"></input>
             <br />
             <div className="button-group">
-                <button type="submit"className="button" onClick={handleRegister}>REGISTER</button>
+                <button type="submit" className="button" onClick={handleRegister}>REGISTER</button>
                 <Link to="/login"><button className="button-text">Already have an account? Sign in.</button></Link>
             </div>
         </form>
