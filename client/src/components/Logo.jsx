3f6b3ade3a97dea235b472a1c9b0664b5faf4f25
Rufat-J <rj.7@live.se>
2023-02-08 23:51:11 +0100

    logo update

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,10 +1,18 @@
 import {Link} from "react-router-dom";
 import React from "react";
+import '../styles/header.css'
 
 export default function () {
     return <Link to={'/'}>
-        <div className="content">
-            <h1>Codinema</h1>
-            <h1>Codinema</h1>
-        </div></Link>
+        <div className="waviy">
+            <span style={{'--i':1}}>C</span>
+            <span style={{'--i':2}}>o</span>
+            <span style={{'--i':3}}>d</span>
+            <span style={{'--i':4}}>i</span>
+            <span style={{'--i':5}}>n</span>
+            <span style={{'--i':6}}>e</span>
+            <span style={{'--i':7}}>m</span>
+            <span style={{'--i':8}}>a</span>
+        </div>
+    </Link>
 }
