df6c2d42025aa2784f3f65c2db5db445a6f0a517
Rufat-J <rj.7@live.se>
2023-02-09 02:01:15 +0100

    added handImg to AccountPage

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,8 +1,10 @@
 import AccountForm from "../components/AccountForm.jsx";
 import '../styles/accountPage.css'
+import handImg from "../images/hand.png"
 
 export default function() {
     return <section id={"account-body"}>
+        <img className='handImg' src={handImg} alt=""/>
         <div className="accountContainer">
         <h2 id={"account-headline"}>Account</h2>
         <AccountForm />
