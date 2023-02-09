e007f20bbfb538cda90fac8127555babac4d6c1c
Rufat-J <rj.7@live.se>
2023-02-09 01:00:25 +0100

    accountPage update

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -3,7 +3,9 @@ import '../styles/accountPage.css'
 
 export default function() {
     return <section id={"account-body"}>
+        <div className="accountContainer">
         <h2 id={"account-headline"}>Account</h2>
         <AccountForm />
+        </div>
     </section>
 }
