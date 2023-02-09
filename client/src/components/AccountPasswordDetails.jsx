22a205c47787e53befd58eb854eff7714d5a3aa3
SimonLeBuono123 <simon.villoresi@gmail.com>
2023-02-09 16:08:15 +0100

    removed required from password

--- client/src/components/AccountPasswordDetails.jsx
+++ client/src/components/AccountPasswordDetails.jsx
@@ -4,6 +4,6 @@ export default function AccountPasswordDetails() {
     return <>
         <label style={{display: " none"}}  htmlFor="password">Current password: </label>
         <input type={"password"} name={"current-password"} id={"current-password"} className={"input-element"}
-              style={{display: " none"}} disabled={true} required/>
+              style={{display: " none"}} disabled={true} />
     </>
 }
