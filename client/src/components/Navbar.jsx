8150c5769129bc4ad057fc819e4f5da5394e4f99
SimonLeBuono123 <simon.villoresi@gmail.com>
2023-02-12 15:40:29 +0100

    Added function that shows first name instead of "account" in navbar
    This is showed only when you are logged else you will see the normal "account" on navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,12 +6,15 @@ import GlobalContext from "../GlobalContext.jsx";
 
 export default function () {
     const {auth} = useContext(GlobalContext)
+
+    const firstName = auth.fullName?.split(" ").splice(0, 1)
+
     return <nav>
         <Link to="movies" id='navbar'>Movies</Link>
         <Link to="tickets" id='navbar'>Tickets</Link>
 
         {auth.loggedIn ?
-            <Link to="user/account" id='navbar'> Account </Link>
+            <Link to="user/account" id='navbar'> {firstName}</Link>
             : <Link to="login" id='navbar'> Account </Link>}
 
     </nav>
